75ba6936c21cdd101a3a677ab33c5dc90cd5b545
wisdomlight <wisdomlight@protonmail.com>
2025-01-17 19:39:33 +0000

    preparing the code for modal pop -up

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const main = document.getElementById('main')
+const modal = document.getElementById('modal')
+// main.addEventListener('click', function(e){
+//     alert(`You clicked on: ${e.target.tagName}`)
+})
 
-main.addEventListener('click', function(e){
-    alert(`You clicked on: ${e.target.tagName}`)
+main.addEventListener('click', function(){
+    //when a element has been clicked
+    //change .modal display property from none to
+    modal.style.display='block'
 })
